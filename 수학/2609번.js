c53514ab660dd4e1b2e3017dfaa84658ed4d11cc
dong1hyun <limd1238@naver.com>
2024-10-07 00:00:01 +0900

    2609번

--- 수학/2609번.js
+++ 수학/2609번.js
@@ -4,24 +4,35 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-const Max = 100000001;
-
+let first = true;
+let n;
+let arr;
+let pn = new Array(1001).fill(true); // 소수만 true로 남겨야됨
+pn[1] = false;
 rl.on('line', (input) => {
-    const line = input.trim().split(' ');
-    const a = +line[0];
-    const b = +line[1];
-    let r1, r2;
-    for(let i = 1; i < Max; i++) {
-        if(a % i === 0 && b & i === 0) {
-            r1 = i;
-        }
-        if(i % a === 0 && i % b === 0) {
-            r2 = i; 
-            break;
-        }
+    if(first) {
+        n = +input;
+        first = false;
+        return;
     }
+    arr = input.trim().split(' ');
 });
 
 rl.on('close', () => {
-  console.log(r1 + '\n' + r2);
+    for(let i = 2; i <= 1000; i++) {
+        for(let j = i * i; j <= 1000; j += i) {
+            if(pn[j]) pn[j] = false;
+        }
+    }
+
+    for(let i = 1; i <= 1000; i++) {
+        if(pn[i]) console.log(i);
+    }
+
+    result = 0;
+    for(let i = 0; i < arr.length; i++) {
+        if(pn[+arr[i]]) result++;
+    }
+
+    console.log(result);
 });
