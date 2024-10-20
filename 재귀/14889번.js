0e248672dc09bbc548d49793236f5fb28a8968c2
dong1hyun <limd1238@naver.com>
2024-10-20 23:55:05 +0900

    14889번

--- 재귀/14889번.js
+++ 재귀/14889번.js
@@ -4,25 +4,55 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-let n, s;
-let arr;
+let n;
+let arr = [];
 let first = true;
-let count = 0;
+let min = 1000000000;
 rl.on('line', (input) => {
-    if(first) {
-        [n, s] = input.trim().split(' ').map(item => +item);
+    if (first) {
+        n = +input;
         first = false;
         return;
     }
-    arr = input.trim().split(' ').map(item => +item);
+    arr.push(input.trim().split(' ').map(item => +item));
 });
 
-let p = [];
-const solve = (start, c) => {
-    
+let squad = [];
+const solve = (c, start) => {
+    if (c === n / 2) {
+        let squad2 = [];
+        let idx = 0;
+        for(let k = 0; k < n; k++) {
+            if(squad[idx] === k) {
+                idx++;
+                continue;
+            }
+            squad2.push(k);
+        }
+        let v = 0, v2 = 0;
+        for (let i = 0; i < n / 2; i++) {
+            const a = squad[i];
+            const a2 = squad2[i];
+            for (let j = 0; j < n / 2; j++) {
+                if (i === j) continue;
+                const b = squad[j];
+                const b2 = squad2[j];
+                v += arr[a][b];
+                v2 += arr[a2][b2];
+            }
+        }
+        const result = Math.abs(v - v2);
+        min = Math.min(min, result);
+        return;
+    }
+    for (let i = start; i < n; i++) {
+        squad.push(i);
+        solve(c + 1, i + 1);
+        squad.pop();
+    }
 }
 
 rl.on('close', () => {
     solve(0, 0);
-    console.log(count);
+    console.log(min);
 });
